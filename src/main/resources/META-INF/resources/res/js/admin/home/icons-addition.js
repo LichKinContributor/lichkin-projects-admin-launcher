2f1622fe8e92917f7389b403ac559a498a302a9a
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-10 19:19:59 +0800

    activiti、app、attendance、pss

---
+++ src/main/resources/META-INF/resources/res/js/admin/home/icons-addition.js
@@ -0,0 +1,17 @@
+$.LKExtendICON({
+  'sysMgmt' : 'cog',
+  'orgMgmt' : 'sitemap',
+
+  'activitiMgmt' : 'braille',
+
+  'appMgmt' : 'mobile-alt',
+
+  'attendanceMgmt' : 'history',
+
+  'pssMgmt' : 'archive',
+  'pssBasicMgmt' : 'cogs',
+  'pssPurchaseMgmt' : 'cart-arrow-down',
+  'pssSellMgmt' : 'shopping-cart',
+  'pssStockMgmt' : 'hockey-puck',
+  'pssReport' : 'chart-line',
+});
