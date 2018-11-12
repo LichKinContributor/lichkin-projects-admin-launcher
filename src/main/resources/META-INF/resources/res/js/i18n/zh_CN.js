d152dfeb0c8269a0f37e419a85e93d614ea0a112
lk-chenshuili <chenshuili@lichkin.com>
2018-11-12 22:28:44 +0800

    初始化数据修改

--- src/main/resources/META-INF/resources/res/js/i18n/zh_CN.js
+++ src/main/resources/META-INF/resources/res/js/i18n/zh_CN.js
@@ -1092,8 +1092,81 @@ $.LKExtendI18N({
     'grid' : {
       'title' : '采购入库单',
 
-      'columns' : {},
-    }
+      'columns' : {
+        'orderNo' : '订单号',
+        'storageName' : '入库仓库',
+        'billDate' : '订单日期',
+        'approvalStatus' : '审批状态',
+        'approvalTime' : '审批时间',
+        'purchaseOrderNo' : '采购订单',
+        'purchaserBillDate' : '采购时间',
+        'purchaseOrderAmount' : '采购金额(元)',
+        'supplierName' : '供应商',
+        'purchaserName' : '采购人',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+        'scanCode' : '扫码',
+        'remarks' : '备注',
+      },
+
+      'addFromPurchaseOrder' : '从采购单录入',
+      'selectPurchaseOrder' : '选择采购单',
+      'add' : '录入订单',
+      'edit' : '编辑订单',
+      'remove' : '删除订单',
+      'submit' : '提交订单',
+      'view' : '查看订单',
+
+      'confirm' : {
+        'remove' : '数据删除后将不能恢复，确认删除么？',
+        'submit' : '数据提交后将不能修改，确认提交么？',
+      },
+
+      'no stored products on current purchase order' : '当前采购单无可入库的产品。',
+      'product not exists' : '此产品无可入库的数量',
+      'only PENDING status can be edit' : '只有草稿状态才可编辑。',
+      'only PENDING status can be remove' : '只有草稿状态才可删除。',
+      'only PENDING status can be submit' : '只有草稿状态才可提交。',
+    },
+
+    'product-grid' : {
+      'title' : '产品列表',
+
+      'columns' : {
+        'productCode' : '产品编码',
+        'productName' : '产品名称',
+        'barcode' : '条形码',
+        'unit' : '单位',
+        'canStockInQty' : '可入库数量',
+        'quantity' : '数量',
+        'product' : '选择产品',
+      },
+
+      'add' : '录入产品',
+      'remove' : '删除产品',
+    },
+
+    'purchaseOrder-grid' : {
+      'title' : '采购订单列表',
+
+      'columns' : {
+        'orderNo' : '订单号',
+        'billDate' : '订单日期',
+        'inventoryStatus' : '入库状态',
+        'INVENTORYSTATUS' : {
+          'NOT' : '未入库',
+          'PART' : '部分入库',
+          'ALL' : '全部入库'
+        },
+        'supplierName' : '供应商',
+        'purchaserName' : '采购人',
+        'orderAmount' : '订单金额',
+        'startDate' : '订单开始日期',
+        'endDate' : '订单结束日期',
+      },
+
+    },
+
   }
 });
 $.LKExtendI18N({
@@ -1210,11 +1283,7 @@ $.LKExtendI18N({
         'billDate' : '盘点日期',
         'storageName' : '盘点仓库',
         'stockCheckCount' : '盘点产品数',
-        'usingStatus' : '在用状态',
-        'USING_STATUS' : {
-          'STAND_BY' : '待用',
-          'USING' : '在用'
-        },
+        'usingStatus' : '盘点状态',
         'approvalStatus' : '审批状态',
         'approvalTime' : '审批时间',
         'startDate' : '订单开始日期',
@@ -1225,7 +1294,7 @@ $.LKExtendI18N({
 
       'add' : '录入订单',
       'edit' : '编辑订单',
-      'hold' : '暂存订单',
+      'hold' : '盘点结束',
       'remove' : '删除订单',
       'submit' : '提交订单',
       'view' : '查看订单',
@@ -1237,11 +1306,13 @@ $.LKExtendI18N({
       },
 
       'please choose the storage' : '请选择盘点仓库。',
+      'product already exists' : '产品已存在。',
       'this product does not exist in the current storage' : '当前仓库不存在此产品。',
-      'only STAND_BY status can be edit' : '只有待用状态才可编辑。',
-      'only STAND_BY status can be hold' : '只有待用状态才可暂存。',
+      'only STAND_BY status can be edit' : '只有盘点中状态才可编辑。',
+      'only STAND_BY status can be hold' : '只有盘点中状态才可盘点结束。',
       'only PENDING status can be remove' : '只有草稿状态才可删除。',
       'only PENDING status can be submit' : '只有草稿状态才可提交。',
+      'only today order can be submit' : '只有当日盘点单才可提交。',
     },
 
     'product-grid' : {
